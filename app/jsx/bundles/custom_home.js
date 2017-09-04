8f6c0b8af9d558918f775c03e25348d6d95d0d78
Chris Schuhmacher <chriss@thelastmile.org>
2017-09-04 08:17:29 -0700

    readding custom_home.js

--- app/jsx/bundles/custom_home.js
+++ app/jsx/bundles/custom_home.js
@@ -2,6 +2,7 @@ import $ from 'jquery'
 
 var roles = ENV.current_user_roles;
 console.log(roles);
+console.log('custom home present');
 
 if(roles.indexOf('student') === -1){
 	$('#reports').show();
