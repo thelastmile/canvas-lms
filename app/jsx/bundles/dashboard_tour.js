dd6058306ff563512ee8a483ca2e0e91eccdf717
Chris Schuhmacher <chriss@thelastmile.org>
2017-09-05 13:07:19 -0700

    adding hopscotch guided tour to student view

---
+++ app/jsx/bundles/dashboard_tour.js
@@ -0,0 +1,60 @@
+import $ from 'jquery'
+
+console.log('getting ready for tour');
+
+// Define the tour!
+var tour = {
+  id: "welcome-page-tour",
+  steps: [
+    {
+      title: "User Account",
+      content: "This is where you login and logout",
+      target: "global_nav_profile_link",
+      placement: "right"
+    },
+    {
+      title: "Home Page",
+      content: "This is your home page for courses and latest news from TLM Global",
+      target: "global_nav_dashboard_link",
+      placement: "right"
+    },
+    {
+      title: "Courses",
+      content: "This is where you'll find courses, modules, and grades",
+      target: "global_nav_courses_link",
+      placement: "right"
+    },
+    {
+      title: "Calender",
+      content: "This is a calender of due dates for assignments, quizzes, and capstone projects",
+      target: "global_nav_calendar_link",
+      placement: "right"
+    },
+    {
+      title: "Inbox",
+      content: "This is your home page for courses and latest news from TLM Global",
+      target: "global_nav_conversations_link",
+      placement: "right"
+    },
+    {
+      title: "Resources",
+      content: "Here are your external resources like GIT, Free Code Camp, Mattermost, and JOLT",
+      target: "global_nav_resources_link",
+      placement: "right"
+    },
+    {
+      title: "Help Videos",
+      content: "If you get lost, check out our selection of Canvas help videos",
+      target: "global_nav_videos_link",
+      placement: "right"
+    }
+  ]
+};
+
+$('.skip-btn').on('click', function(){
+	$('.welcome-page').hide();
+})
+
+$('.tour-btn').on('click', function(){
+	hopscotch.startTour(tour);	
+});
